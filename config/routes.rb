5be3d768cb45e0ed76bbb5026111a8c285daca7a
robin <robin.j.soto@gmail.com>
2020-09-11 17:09:39 -0700

    adds REST conventions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
+    get "/products" => "products#show"
     get "/products/:id" => "products#show"
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
