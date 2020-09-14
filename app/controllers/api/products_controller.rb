4728c5a8ae5bf104824800f8a9af5325973c50c0
robin <robin.j.soto@gmail.com>
2020-09-14 12:34:33 -0700

    adds config for frontend and model methods to manipulate data

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -24,18 +24,24 @@ class Api::ProductsController < ApplicationController
   # In backend, pass the POST key-value pairings through Insomnia
 
   def update
-    @product = Product.find_by(params[:id])
+    @product = Product.find(params[:id])
 
-    @product.name = params[:name],
-    @product.price = params[:price],
-    @product.image_path = params[:image_path],
-    @product.description = params[:description]
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_path = params[:image_path] || @product.image_path
+    @product.description = params[:description] || @product.description
 
     @product.save
     render "show.json.jb"
   end
   # In backend, pass the PATCH key-value pairings through Insomnia
-  # If you don't pass something in the params hash, you're overriding the existing code with `null`
+  # If you don't pass something in the params hash, you're overriding the existing code with `null` -- use || to selectively update.
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Recipe successfully destroyed!"}
+  end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     get "/products/:id" => "products#show"
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
   end
 end
