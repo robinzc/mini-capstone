200d7a77c09ed2744589a1419b4b093f694fce94
robin <robin.j.soto@gmail.com>
2020-09-18 11:37:39 -0700

    comments out validations to avoid issues

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
 
-  validates :name, presence: true, presence: true
-  validates :image_path, presence: true
-  validates :description, length: { in: 10..500 }
-  validates :image_path, presence: true
-  validates :price, numericality: { greater_than: 0 }
-  validates :inventory, numericality: { greater_than: 0 }
+  # validates :name, presence: true, presence: true
+  # validates :image_path, presence: true
+  # validates :description, length: { in: 10..500 }
+  # validates :image_path, presence: true
+  # validates :price, numericality: { greater_than: 0 }
+  # validates :inventory, numericality: { greater_than: 0 }
 
 #Class method
   # scope :name_search, -> (search_term) { where ("name iLIKE ?", "%#{search_term}%") }
