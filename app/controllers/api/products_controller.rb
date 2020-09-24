7f0f69334ee99b00b4363d3068976d8192934548
robin <robin.j.soto@gmail.com>
2020-09-24 16:25:03 -0700

    adds associations and index parameters

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,10 +3,18 @@ class Api::ProductsController < ApplicationController
   before_action :authenticate_admin, except: [:index, :show]
 
   def index
-    if current_user
-      @products = Product.all 
-      render "index.json.jb"
+    @products = Product
+      .name_search(params[:search])
+      .discounted(params[:discount])
+      .sorted(params[:sort], params[sort_order])
+
+    if params[:category]
+      category = Category.find_by(name: params[:category])
+      @products = category.products
     end
+    render "index.json.jb"
+  end
+  
     # if params[:search]
     #   @products = @products.where("name iLIKE ?", "%#{params[:search]}%")
     # end 
@@ -24,7 +32,7 @@ class Api::ProductsController < ApplicationController
     # else
     #   @products = @products.order(:id)
     # end
-  end
+    
 
   def show
     product_id = params[:id]
