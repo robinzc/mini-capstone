1dfa777f526415597c35cb967f7503e0bbaff693
robin <robin.j.soto@gmail.com>
2020-10-13 13:39:04 -0700

    pre-heroku commitz

---
+++ public/index.js
@@ -0,0 +1,7 @@
+var message = "Helloooo";
+console.log(message);
+
+axios.get("http://localhost:3000/api/products").then(function (response) {
+  var products = response.data;
+  console.log(products);
+});
